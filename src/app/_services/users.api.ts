6398421840f7c169ca276d41b7643cac3efff509
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2024-06-25 20:29:07 -0600

    first commit

---
+++ src/app/_services/users.api.ts
@@ -0,0 +1,14 @@
+import axiosWrapper from "./axiosWrapper";
+
+const api = axiosWrapper({
+  baseURL: "/users",
+});
+
+const findAll = async () => {
+  const { data } = await api.get<{ message: string }>("");
+  return data;
+};
+
+export const UsersApi = {
+  findAll,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
+
+import { Toaster } from "sonner";
+
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +23,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+        <Toaster position="top-right" expand={true} richColors closeButton />
+      </body>
     </html>
   );
 }
